abb55d0a58fafba09c8e5f42c76ab6861ae4ef6c
Derek Kastner <dkastner@gmail.com>
2010-07-06 18:31:33 -0400

    No data_miner

--- features/fixtures/flight_record.rb
+++ features/fixtures/flight_record.rb
@@ -23,33 +23,6 @@ class FlightRecord < ActiveRecord::Base
                 :distance_estimate => 2077.4455, # SELECT SUM(passengers * distance) / SUM(passengers) FROM flight_segments;
                 :load_factor => lambda { FlightSegment.fallback.load_factor }
 
-  data_miner do
-    schema do
-      string   'origin_airport_id'
-      string   'destination_airport_id'
-      integer  'trips'
-      integer  'emplanements_per_trip'
-      float    'distance_estimate'
-      string   'distance_class_id'
-      string   'aircraft_id'
-      string   'aircraft_class_id'
-      string   'propulsion_id'
-      string   'fuel_type_id'
-      string   'airline_id'
-      string   'seat_class_id'
-      integer  'seats_estimate'
-      float    'load_factor'
-      string   'domesticity_id'
-      date     'date'
-      integer  'year'
-      time     'time_of_day'
-    end
-    
-    process "pull dependencies" do
-      run_data_miner_on_belongs_to_associations
-    end
-  end
-  
   characterize do
     has :date, :trumps => :year
     has :year
