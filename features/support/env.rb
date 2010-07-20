05e3b3666d13d5481a65f1aeab7ecb736264c511
Derek Kastner <dkastner@gmail.com>
2010-07-20 15:51:09 -0400

    Updated call to Sniff.init

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,15 +5,4 @@ require 'cucumber'
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 
 require 'sniff'
-
-Sniff.init File.join(File.dirname(__FILE__), '..', '..')
-
-## How to clean your database when transactions are turned off. See
-## http://github.com/bmabey/database_cleaner for more info.
-#if defined?(ActiveRecord::Base)
-#  begin
-#    require 'database_cleaner'
-#    DatabaseCleaner.strategy = :truncation
-#  rescue LoadError => ignore_if_database_cleaner_not_present
-#  end
-#end
+Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :air
