9cc48f8affb34dc70144504c43b08ca7628e058e
Ian Hough <ian@brighterplanet.com>
2010-10-14 15:31:55 -0700

    Don't use andand b/c fallbacks should never be nil, and if they are we want stuff to go wrong so we notice

--- lib/flight/carbon_model.rb
+++ lib/flight/carbon_model.rb
@@ -207,7 +207,7 @@ module BrighterPlanet
             end
             
             quorum 'from aircraft class', :needs => :aircraft_class do |characteristics|
-              characteristics[:aircraft_class].seats
+              characteristics[:aircraft_class].seats_before_type_cast
             end
             
             quorum 'default' do
@@ -231,7 +231,7 @@ module BrighterPlanet
             end
             
             quorum 'default' do
-              FlightSegment.fallback.andand.freight_share
+              FlightSegment.fallback.freight_share
             end
           end
           
@@ -277,7 +277,7 @@ module BrighterPlanet
             end
             
             quorum 'default' do
-              FlightSeatClass.fallback.andand.multiplier
+              FlightSeatClass.fallback.multiplier
             end
           end
           
