229a590c5d75499b277c9a7f3bde820aaabb0f30
Ian Hough <ian@brighterplanet.com>
2010-10-14 15:38:27 -0700

    Re-order characteristics to follow carbon_model.rb, and get rid of unecessary ones

--- lib/flight/characterization.rb
+++ lib/flight/characterization.rb
@@ -6,26 +6,24 @@ module BrighterPlanet
       def self.included(base)
         base.send :include, Characterizable
         base.characterize do
-          has :date, :trumps => :year
-          has :year
-          has :time_of_day
+          has :aviation_multiplier
+          has :distance_estimate, :trumps => :distance_class, :measures => :length, :precision => 0
+          has :distance_class
+          has :fuel_type
+          has :seats_estimate, :range => 1..500
+          has :load_factor, :measures => :percentage
+          has :trips
+          has :seat_class
+          has :country
+          has :date
           has :origin_airport do |origin_airport|
             origin_airport.reveals :destination_airport, 
-              :trumps => [:distance_class, :domesticity, :distance_estimate]
+              :trumps => [:distance_class, :distance_estimate, :country]
           end
-          has :distance_class
-          has :distance_estimate, :trumps => :distance_class, :measures => :length, :precision => 0
+          has :aircraft, :trumps => [:aircraft_class, :seats_estimate, :fuel_type]
+          has :aircraft_class, :trumps => :fuel_type
           has :airline
-          has :trips
-          has :emplanements_per_trip
-          has :seat_class
-          has :load_factor, :measures => :percentage
-          has :seats_estimate, :range => 1..500
-          has :aircraft_class, :trumps => [:propulsion, :fuel_type]
-          has :aircraft, :trumps => [:propulsion, :aircraft_class, :seats_estimate, :fuel_type]
-          has :propulsion, :trumps => :fuel_type
-
-          has :creation_date, :hidden => true
+          has :segments_per_trip
         end
         base.add_implicit_characteristics
       end
