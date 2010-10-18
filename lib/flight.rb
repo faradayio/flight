3cd77b95907170e5d0d1bd740189e98d6608b2e3
Derek Kastner <dkastner@gmail.com>
2010-10-18 15:06:37 -0400

    Don't need Flight.flight_model anymore

--- lib/flight.rb
+++ lib/flight.rb
@@ -3,17 +3,5 @@ require 'emitter'
 module BrighterPlanet
   module Flight
     extend BrighterPlanet::Emitter
-
-    class << self
-      def flight_model
-        if Object.const_defined? 'Flight'
-          ::Flight
-        elsif Object.const_defined? 'FlightRecord'
-          FlightRecord
-        else
-          raise 'There is no flight model'
-        end
-      end
-    end
   end
 end
