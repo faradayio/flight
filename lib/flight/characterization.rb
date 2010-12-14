8f4c6bdb0a4a828437471b3046ac3299c2a223a3
Seamus Abshere <seamus@abshere.net>
2010-12-14 10:57:53 -0600

    don't need trumping or measurement information like this

--- lib/flight/characterization.rb
+++ lib/flight/characterization.rb
@@ -6,22 +6,21 @@ module BrighterPlanet
       def self.included(base)
         base.send :include, Characterizable
         base.characterize do
+          # sabshere 12/13/10 should this really be a user input?
           has :aviation_multiplier
-          has :distance_estimate, :trumps => :distance_class, :measures => :length, :precision => 0
+          has :distance_estimate
           has :distance_class
           has :fuel_type
-          has :seats_estimate, :range => 1..500
-          has :load_factor, :measures => :percentage
+          has :seats_estimate
+          has :load_factor
           has :trips
           has :seat_class
           has :country
           has :date
-          has :origin_airport do |origin_airport|
-            origin_airport.reveals :destination_airport, 
-              :trumps => [:distance_class, :distance_estimate, :country]
-          end
-          has :aircraft, :trumps => [:aircraft_class, :seats_estimate, :fuel_type]
-          has :aircraft_class, :trumps => :fuel_type
+          has :origin_airport
+          has :destination_airport
+          has :aircraft
+          has :aircraft_class
           has :airline
           has :segments_per_trip
         end
