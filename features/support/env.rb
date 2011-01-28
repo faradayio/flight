3d27b80d9e618bb9f5a6264f2878318a63a2060b
Derek Kastner <dkastner@gmail.com>
2011-01-28 13:09:50 -0500

    Get rid of crazy mysql output

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,5 +4,8 @@ Bundler.setup
 require 'cucumber'
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 
+require 'data_miner'
+DataMiner.logger = Logger.new(nil)
+
 require 'sniff'
 Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => [:air, :locality, :fuel], :cucumber => true
