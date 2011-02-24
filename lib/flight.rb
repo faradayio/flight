a2ee13ad918f5ac4d1f38ccc7464701d1adfb87c
Ian Hough <ian@brighterplanet.com>
2011-02-23 18:03:36 -0800

    Add scope

--- lib/flight.rb
+++ lib/flight.rb
@@ -3,5 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module Flight
     extend BrighterPlanet::Emitter
+    scope 'The flight emission estimate is the anthropogenic emissions per passenger from aircraft fuel combustion and radiative forcing. It includes CO2 emissions from combustion of non-biogenic fuel and extra forcing effects of high-altitude combustion.'
   end
 end
