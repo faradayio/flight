393bee1a572599e183e4ce0792d7b98842ebdd44
Ian Hough <ian@brighterplanet.com>
2011-05-20 00:07:09 -0400

    Set up fuzzy matches between Aircraft and Airline

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,3 +6,9 @@ require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumb
 
 require 'sniff'
 Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => [:air, :locality, :fuel], :cucumber => true, :logger => 'log/test_log.txt'
+
+# Set up fuzzy matching between Aircraft and FlightSegment for testing
+require 'loose_tight_dictionary'
+require 'earth/air/flight_segment/data_miner'
+step = FlightSegment.data_miner_config.steps.detect { |s| s.class == DataMiner::Process and s.block_description =~ /cache fuzzy/i }
+step.run
