505d25a6bc94e0a68bca8f53e536c74f086718cb
Ian <ian@worklap.local>
2011-08-11 12:09:12 +0200

    Set up aircraft-flight segment fuzzy matching and calculate averages for tests.

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,20 +7,14 @@ require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumb
 require 'sniff'
 Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => [:air, :locality, :fuel], :cucumber => true, :logger => 'log/test_log.txt'
 
-# Set up fuzzy matching between Aircraft and FlightSegment for testing
+# Set up fuzzy matching between Aircraft and FlightSegment and derive Aircraft seats and passengers from FlightSegment
 require 'loose_tight_dictionary'
 require 'earth/air/flight_segment/data_miner'
-step = Aircraft.data_miner_config.steps.detect { |s| s.class == DataMiner::Process and s.description =~ /cache fuzzy/i }
-step.run
-
-# Derive characteristics of Aircraft from flight_segments
 require 'earth/air/aircraft/data_miner'
-step = Aircraft.data_miner_config.steps.detect { |s| s.class == DataMiner::Process and s.description =~ /derive some average/i }
-step.run
+FlightSegment.data_miner_config.steps.clear
+Aircraft.update_averages!
 
-# Derive characteristics of AircraftClass from aircraft
+# Derive AircraftClass fuel use equations and seats from Aircraft
 require 'earth/air/aircraft_class/data_miner'
-step = AircraftClass.data_miner_config.steps.detect { |s| s.class == DataMiner::Process and s.description =~ /Derive aircraft classes/i }
-step.run
-step = AircraftClass.data_miner_config.steps.detect { |s| s.class == DataMiner::Process and s.description =~ /Derive some average/i }
-step.run
+Aircraft.data_miner_config.steps.clear
+AircraftClass.run_data_miner!
