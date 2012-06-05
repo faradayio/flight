572a2ea2f5c108a2cc3dc4302fb21f3acf759b53
Seamus Abshere <seamus@abshere.net>
2012-06-05 11:03:12 -0500

    clean up methods and memoize the heavy ones

--- lib/flight/impact_model/flight_segment_cohort.rb
+++ lib/flight/impact_model/flight_segment_cohort.rb
@@ -14,19 +14,26 @@ module BrighterPlanet
         end
         
         attr_reader :characteristics
-        attr_reader :subselect_sql
         attr_reader :table_name
+        attr_reader :origin_airport
+        attr_reader :destination_airport
         
         # We pull values out of charisma curations here
         # TODO: don't make up for charisma's probs
         def initialize(characteristics)
-          @table_mutex = ::Mutex.new
+          @select_manager_mutex = Mutex.new
+          @subquery_sql_mutex = Mutex.new
+
+          @table_name = "flight_segment_cohort_#{Kernel.rand(1e11)}"
           @characteristics = characteristics.inject({}) do |memo, (k, v)|
             if vv = v.respond_to?(:value) ? v.value : v
               memo[k] = vv
             end
             memo
           end
+          @origin_airport = characteristics[:origin_airport]
+          @destination_airport = characteristics[:destination_airport]
+          @covered_by_bts_query = (origin_airport.try(:country_iso_3166_code) == 'US' or destination_airport.try(:country_iso_3166_code) == 'US')
         end
         
         def valid?
@@ -37,17 +44,17 @@ module BrighterPlanet
         end
         
         def weighted_average(*args)
-          table.weighted_average(*args)
+          select_manager.weighted_average(*args)
         end
         
         def to_sql
-          subselect_sql
+          subquery_sql
         end
         
         def count
-          count_table = table.dup
-          count_table.projections = [Arel.sql('COUNT(*)')]
-          FlightSegment.connection.select_value count_table.to_sql
+          counter = select_manager.dup
+          counter.projections = [Arel.sql('COUNT(*)')]
+          FlightSegment.connection.select_value counter.to_sql
         end
 
         def as_json(*)
@@ -55,15 +62,29 @@ module BrighterPlanet
         end
 
         def cleanup
-          FlightSegment.connection.execute %{
-            DROP TABLE #{table_name}
-          }
+          execute %{ DROP TABLE #{table_name} }
         end
         
         private
 
+        def connection
+          FlightSegment.connection
+        end
+
+        def execute(sql)
+          connection.execute sql
+        end
+
+        def sqlite?
+          connection.adapter_name =~ /sqlite/i
+        end
+
+        def mysql?
+          connection.adapter_name =~ /mysql/i
+        end
+
         def provided
-          {
+          @provided ||= {
             :aircraft_description => characteristics[:aircraft].try(:flight_segments_foreign_keys),
             :airline_name => characteristics[:airline].try(:name),
             :origin_airport_iata_code => origin_airport.try(:iata_code),
@@ -89,7 +110,7 @@ module BrighterPlanet
         end
 
         def covered_by_bts?
-          origin_airport.try(:country_iso_3166_code) == "US" or destination_airport.try(:country_iso_3166_code) == "US"
+          @covered_by_bts_query
         end
 
         def cohort_from_source(source, priority)
@@ -116,21 +137,9 @@ module BrighterPlanet
           cohort_from_source 'ICAO TFS', [:origin_airport_city, :destination_airport_city, :aircraft_description, :airline_name]
         end
 
-        def origin_airport
-          characteristics[:origin_airport]
-        end
-
-        def destination_airport
-          characteristics[:destination_airport]
-        end
-
-        def table
-          return @table if @calculated == true
-          @table_mutex.synchronize do
-            return @table if @calculated == true
-            @calculated = true
-
-            subselect = if origin_airport.present? and destination_airport.present?
+        def subquery_sql
+          @subquery_sql || @subquery_sql_mutex.synchronize do
+            @subquery_sql ||= if origin_airport.present? and destination_airport.present?
               if covered_by_bts?
                 # NOTE: It's possible that the origin/destination pair won't appear in our database and we'll end up using a
                 # cohort based just on origin. If that happens, even if the origin is not in the US we still don't want to use
@@ -142,40 +151,36 @@ module BrighterPlanet
               end
             else
               bts_cohort.union icao_cohort
+            end.to_sql
+          end
+        end
+
+        def select_manager
+          @select_manager || @select_manager_mutex.synchronize do
+            @select_manager ||= begin
+              populated = false
+
+              if sqlite?
+                populated = true
+                execute %{ CREATE TEMPORARY TABLE #{table_name} AS SELECT * FROM (#{subquery_sql}) }
+              else
+                execute %{ CREATE TEMPORARY TABLE #{table_name} LIKE #{FlightSegment.quoted_table_name} }
+              end
+
+              if mysql?
+                execute %{ ALTER TABLE #{table_name} ENGINE=MEMORY }
+              end
+
+              unless populated
+                execute %{ INSERT INTO #{table_name} SELECT * FROM (#{subquery_sql}) }
+              end
+
+              if mysql?
+                execute %{ ANALYZE TABLE #{table_name} }
+              end
+
+              Arel::SelectManager.new FlightSegment, Arel::Table.new(table_name)
             end
-            
-            @subselect_sql = '(' + subselect.to_sql + ')'
-            c = FlightSegment.connection
-            @table_name = "flight_segment_cohort_#{::Kernel.rand(1e11)}"
-            populated = false
-            if c.adapter_name =~ /sqlite/i
-              populated = true
-              c.execute %{
-                CREATE TEMPORARY TABLE #{table_name} AS
-                SELECT * FROM #{subselect_sql}
-              }
-            else
-              c.execute %{
-                CREATE TEMPORARY TABLE #{table_name} LIKE #{FlightSegment.quoted_table_name}
-              }
-            end
-            if c.adapter_name =~ /mysql/i
-              c.execute %{
-                ALTER TABLE #{table_name} ENGINE=MEMORY
-              }
-            end
-            unless populated
-              c.execute %{
-                INSERT INTO #{table_name}
-                SELECT * FROM #{subselect_sql}
-              }
-            end
-            if c.adapter_name =~ /mysql/i
-              c.execute %{
-                ANALYZE TABLE #{table_name}
-              }
-            end
-            @table = Arel::SelectManager.new FlightSegment, Arel::Table.new(table_name)
           end
         end
       end
--- lib/flight/impact_model/fuel_use_equation/derived.rb
+++ lib/flight/impact_model/fuel_use_equation/derived.rb
@@ -6,7 +6,7 @@ module BrighterPlanet
           attr_reader :flight_segment_cohort
 
           def initialize(flight_segment_cohort)
-            @calculate_mutex = ::Mutex.new
+            @calculate_mutex = Mutex.new
             @flight_segment_cohort = flight_segment_cohort
           end
           
@@ -31,6 +31,22 @@ module BrighterPlanet
           end
         
           private
+
+          def connection
+            Aircraft.connection
+          end
+
+          def execute(sql)
+            connection.execute sql
+          end
+
+          def sqlite?
+            connection.adapter_name =~ /sqlite/i
+          end
+
+          def mysql?
+            connection.adapter_name =~ /mysql/i
+          end
         
           def calculate!
             return if @calculated == true
@@ -38,11 +54,9 @@ module BrighterPlanet
               return if @calculated == true
               @calculated = true
 
-              c = ActiveRecord::Base.connection
+              table_name = "flight_fuel_use_coefficients_#{Kernel.rand(1e11)}"
 
-              table_name = "flight_fuel_use_coefficients_#{::Kernel.rand(1e11)}"
-
-              c.execute %{
+              execute %{
                 CREATE TEMPORARY TABLE #{table_name} (
                   aircraft_description VARCHAR(255),
                   m3 FLOAT,
@@ -54,21 +68,19 @@ module BrighterPlanet
               }
 
               # make this run faster if you're on mysql
-              if c.adapter_name =~ /mysql/i
-                c.execute %{
-                  ALTER TABLE #{table_name} ENGINE=MEMORY
-                }
+              if mysql?
+                execute %{ ALTER TABLE #{table_name} ENGINE=MEMORY }
               end
 
               # - For each unique aircraft description:
               # - 1. look up all the aircraft it refers to
               # - 2. average those aircraft's fuel use coefficients
               # - 3. store the resulting values in the temporary table along with the unique aircraft_description
-              c.execute %{
+              execute %{
                 INSERT INTO #{table_name} (aircraft_description, m3, m2, m1, b)
                   SELECT fz.b, AVG(ac.m3), AVG(ac.m2), AVG(ac.m1), AVG(ac.b)
-                  FROM #{::FuzzyMatch::CachedResult.quoted_table_name} AS fz
-                    INNER JOIN #{::Aircraft.quoted_table_name} AS ac
+                  FROM #{FuzzyMatch::CachedResult.quoted_table_name} AS fz
+                    INNER JOIN #{Aircraft.quoted_table_name} AS ac
                     ON fz.a = ac.description AND fz.a_class = 'Aircraft' AND fz.b_class = 'FlightSegment'
                   WHERE fz.b IN (
                     SELECT DISTINCT #{flight_segment_cohort.table_name}.aircraft_description FROM #{flight_segment_cohort.table_name}
@@ -80,7 +92,7 @@ module BrighterPlanet
               # - 1. look up all the flight segments that match the aircraft description
               # - 2. sum passengers across those flight segments
               # - 3. store the resulting value in the temporary table
-              c.execute %{
+              execute %{
                 UPDATE #{table_name}
                 SET passengers = (
                   SELECT SUM(passengers)
@@ -90,7 +102,7 @@ module BrighterPlanet
               }
 
               # - Calculate the average of the coefficients in the temporary table, weighted by passengers
-              row = c.select_one %{
+              row = connection.select_one %{
                 SELECT
                   SUM(1.0 * m3 * passengers)/SUM(passengers) AS avg_m3,
                   SUM(1.0 * m2 * passengers)/SUM(passengers) AS avg_m2,
@@ -107,9 +119,7 @@ module BrighterPlanet
 
               @m3, @m2, @m1, @b = row['avg_m3'], row['avg_m2'], row['avg_m1'], row['avg_b']
 
-              c.execute %{
-                DROP TABLE #{table_name}
-              }
+              execute %{ DROP TABLE #{table_name} }
             end
           end
         end
